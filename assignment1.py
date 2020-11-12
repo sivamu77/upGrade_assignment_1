cb333d55f3cf769fc1da78cb056ac392951d7310
sivamu77 <61821579+sivamu77@users.noreply.github.com>
2020-11-12 22:57:37 +0530

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,7 +1,6 @@
 # 1.B(RAINBOW)
 # 2. print("LETS UPGRADE")
 #3.
-"""
 cost_Price=int(input())
 Selling_price=int(input())
 if (cost_Price<Selling_price):
@@ -10,7 +9,7 @@ elif(cost_Price>Selling_price):
     print("Loss")
 else:
     print("Neither")
-"""
+
 # 4.
 ip=int(input())
 print(ip*80)
