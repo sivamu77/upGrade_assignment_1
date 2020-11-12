4cd82e891b34f7d6fdd4b040281809a534efca41
sivamu77 <61821579+sivamu77@users.noreply.github.com>
2020-11-12 22:50:05 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,16 @@
+# 1.B(RAINBOW)
+# 2. print("LETS UPGRADE")
+#3.
+"""
+cost_Price=int(input())
+Selling_price=int(input())
+if (cost_Price<Selling_price):
+    print("Profit")
+elif(cost_Price>Selling_price):
+    print("Loss")
+else:
+    print("Neither")
+"""
+# 4.
+ip=int(input())
+print(ip*80)
